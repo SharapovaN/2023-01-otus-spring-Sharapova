f9b7f9e7aa06839d3501c9c0fedd4390069942c4
Natalia <n.sharapova@netris.ru>
2023-03-13 21:46:19 +0300

    Comments added

--- 02-library-app/src/main/resources/data.sql
+++ 02-library-app/src/main/resources/data.sql
@@ -1,9 +1,9 @@
-INSERT INTO authors (author_name, author_surname) VALUES ('Aleksandr', 'Pushkin');
-INSERT INTO authors (author_name, author_surname) VALUES ('John', 'Tolkien');
+INSERT INTO authors (author_name, author_surname)
+VALUES ('Aleksandr', 'Pushkin'), ('John', 'Tolkien');
 
-INSERT INTO genres (genre_name) VALUES ('Historical Novel');
-INSERT INTO genres (genre_name) VALUES ('Fantasy');
+INSERT INTO genres (genre_name)
+VALUES ('Historical Novel'), ('Fantasy');
 
-INSERT INTO books (book_name, author_id, genre_id) VALUES ('Captains daughter', 1, 1);
-INSERT INTO books (book_name, author_id, genre_id) VALUES ('Lord Of The Rings', 2, 2);
+INSERT INTO books (book_name, author_id, genre_id)
+VALUES ('Captains daughter', 1, 1), ('Lord Of The Rings', 2, 2);
 
