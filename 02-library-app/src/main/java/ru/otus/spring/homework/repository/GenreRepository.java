32f3d83665ef535081b89597e56c08039d6afdcc
Natalia <n.sharapova@netris.ru>
2023-03-23 11:42:36 +0300

    propagation = Propagation.SUPPORTS added

--- 02-library-app/src/main/java/ru/otus/spring/homework/repository/GenreRepository.java
+++ 02-library-app/src/main/java/ru/otus/spring/homework/repository/GenreRepository.java
@@ -1,7 +1,10 @@
 package ru.otus.spring.homework.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import ru.otus.spring.homework.model.Genre;
 
+@Transactional(propagation = Propagation.SUPPORTS)
 public interface GenreRepository extends JpaRepository<Genre, Long> {
 }
