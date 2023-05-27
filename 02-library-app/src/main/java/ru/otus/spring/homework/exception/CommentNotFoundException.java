cb6a4b32477012b919b9096c0f0d9f85e811d9d9
Natalia <n.sharapova@netris.ru>
2023-05-27 13:56:18 +0300

    Imports fixed

--- 02-library-app/src/main/java/ru/otus/spring/homework/exception/CommentNotFoundException.java
+++ 02-library-app/src/main/java/ru/otus/spring/homework/exception/CommentNotFoundException.java
@@ -1,8 +1,5 @@
 package ru.otus.spring.homework.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 public class CommentNotFoundException extends ResourceNotFoundException {
 
     public CommentNotFoundException(Long id) {
