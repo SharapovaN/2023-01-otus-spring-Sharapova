ec7dcabe05b570ded0cbb160c5de8d754fc4725c
nsharapova <natalia.sharapova@itentika.ru>
2023-06-19 11:00:26 +0300

    Spring batch

---
+++ 05-spring-batch/src/main/java/ru/otus/spring/homework/Main.java
@@ -0,0 +1,12 @@
+package ru.otus.spring.homework;
+
+import com.github.cloudyrock.spring.v5.EnableMongock;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableMongock
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
